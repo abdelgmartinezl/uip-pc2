d231b5171b0fec6daae069f8e09a2672774fe860
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-27 20:11:06 -0500

    Ejemplo de tipos de datos

---
+++ Ejemplos/TiposDeDatos/src/pa/edu/uip/Main.java
@@ -0,0 +1,35 @@
+package pa.edu.uip;
+
+/*
+Este es un comentario de varias lineas
+porque a veces los comentarios son
+de varias lineas (?)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bievenidos al mundo de Java!");
+
+        // TIPOS DE ENTEROS
+        byte d;
+        short b;
+        int a = 0;
+        long c;
+
+        // TIPOS DE DECIMALES
+        float e = 0.0f;
+        double f = 0.0;
+
+        // CARACTERES
+        char g = 'a';
+        // CADENAS
+        String h = "a";
+
+        // BOOLEANO
+        boolean i = true;
+        i = false;
+
+        System.out.println(a);
+    }
+}
