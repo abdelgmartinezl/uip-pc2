168e99c258022932f6b21bf69c2736ebeaa965b1
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-28 20:12:54 -0500

    Lectura de datos

--- Ejemplos/EntradaDatos/src/pa/edu/uip/Main.java
+++ Ejemplos/EntradaDatos/src/pa/edu/uip/Main.java
@@ -1,8 +1,15 @@
 package pa.edu.uip;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        // Impresion en pantalla
+        System.out.println("Lectura de Datos");
+
+        // Lectura por teclado
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
     }
 }
