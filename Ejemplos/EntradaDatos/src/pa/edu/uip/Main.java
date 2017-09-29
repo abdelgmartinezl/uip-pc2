c62c4bbeabaab13da7565fe28886a0c9c131a506
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-09-28 20:52:32 -0500

    Ciclos.

--- Ejemplos/EntradaDatos/src/pa/edu/uip/Main.java
+++ Ejemplos/EntradaDatos/src/pa/edu/uip/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String nombre;
         int edad;
+        double salario;
+        String trabaja;
 
         // Impresion en pantalla
         System.out.println("Lectura de Datos");
@@ -24,7 +26,16 @@ public class Main {
         } else if (edad == 18) {
             System.out.println("Ya no eres tan lapecillo!");
         } else {
-            System.out.println("Larga a estudiar!");
+            System.out.println("Larga a estudiar! O a trabajar (?)");
+            System.out.print("Trabajas? (S/N): ");
+            trabaja = br.readLine();
+            if (trabaja.toUpperCase().equals("S")) {
+                System.out.print("Salario: ");
+                salario = Double.parseDouble(br.readLine());
+                System.out.printf("Te llamas %s y ganas $%.2f", nombre, salario);
+            } else {
+                System.out.println("Larga a estudiar! O a buscar trabajo!");
+            }
         }
 
     }
