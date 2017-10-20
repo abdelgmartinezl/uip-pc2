681c834ecf7e94a08b51fc28439a2ed48d33c58b
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-19 19:05:08 -0500

    Sistema de inventario

--- Ejemplos/OOP/src/no/tengo/naa/Articulo.java
+++ Ejemplos/OOP/src/no/tengo/naa/Articulo.java
@@ -1,4 +1,17 @@
 package no.tengo.naa;
 
 public class Articulo {
+    private int id;
+    private String nombre;
+    private float dimensiones;
+    private float precioUnitario;
+    private int cantidad;
+
+    public int vender() {
+        return 0;
+    }
+
+    public int almacenar() {
+        return 0;
+    }
 }
