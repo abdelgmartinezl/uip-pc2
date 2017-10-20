845e31383146ec3a35b71cd7f8c7dfa4ab90deaf
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-19 19:28:11 -0500

    Sistema de inventario

--- Ejemplos/OOP/src/no/tengo/naa/Main.java
+++ Ejemplos/OOP/src/no/tengo/naa/Main.java
@@ -1,19 +1,33 @@
 package no.tengo.naa;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("¡Bienvenido a No Tengo Naa!");
-        System.out.println("SISTEMA DE INVENTARIO - MENÚ");
-        System.out.println("1. Cotizar producto");
-        System.out.println("2. Solicitar producto");
-        System.out.println("3. Crear nuevo articulo");
-        System.out.println("4. Clasificar");
-        System.out.println("5. Eliminar");
-        System.out.println("6. Compra-Venta");
-        System.out.println("7. Crear pedido");
-        System.out.println("8. Costo de producto");
-        System.out.println("9. Salir");
-        System.out.print("OPCION: ");
+        int opcion = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (opcion != 9) {
+            System.out.println("¡Bienvenido a No Tengo Naa!");
+            System.out.println("SISTEMA DE INVENTARIO - MENÚ");
+            System.out.println("1. Cotizar producto\n2. Solicitar producto");
+            System.out.println("3. Crear nuevo articulo\n4. Clasificar");
+            System.out.println("5. Eliminar\n6. Compra-Venta");
+            System.out.println("7. Crear pedido\n8. Costo de producto");
+            System.out.println("9. Salir");
+            System.out.print("OPCION: ");
+            try {
+                opcion = Integer.parseInt(br.readLine());
+            } catch (IOException e) {
+                System.out.println("NO FUNCIONA EL TECLADO");
+            } catch (NumberFormatException e) {
+                System.out.println("DATA BASURA");
+            }
+        }
+
+        System.out.println("Vamos pa lante!");
     }
 }
