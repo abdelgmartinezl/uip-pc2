5631d49013f526d14940dce91a8d2f68c8983a64
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-20 20:06:31 -0500

    Orientación a objetos

---
+++ Ejemplos/TrabajaVago/src/com/trabajavago/Tarea.java
@@ -0,0 +1,12 @@
+package com.trabajavago;
+
+public class Tarea {
+    private String nombre;
+    private String fecha_tope;
+    private String estado;
+    private String criticidad;
+
+    public void completar() {
+        
+    }
+}
