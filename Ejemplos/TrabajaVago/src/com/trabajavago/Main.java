812aa37cc639af02a9ad627597bd6dec0cfcc3c7
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-20 20:30:24 -0500

    Orientación a objetos

--- Ejemplos/TrabajaVago/src/com/trabajavago/Main.java
+++ Ejemplos/TrabajaVago/src/com/trabajavago/Main.java
@@ -1,8 +1,17 @@
 package com.trabajavago;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int opcion;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("APP: TRABAJAVAGO");
+        System.out.println("1. Agregar\n2. Ver\n3. Completar\n4. Salir");
+        System.out.print("Opcion: ");
+        opcion = Integer.parseInt(br.readLine());
     }
 }
