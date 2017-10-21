c41cb3803aa12028325265782b5fc99fa87217d1
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-20 21:00:40 -0500

    Plantilla de menú

--- Ejemplos/TrabajaVago/src/com/trabajavago/Main.java
+++ Ejemplos/TrabajaVago/src/com/trabajavago/Main.java
@@ -1,17 +1,44 @@
 package com.trabajavago;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) {
-        int opcion;
+        byte opcion = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("APP: TRABAJAVAGO");
-        System.out.println("1. Agregar\n2. Ver\n3. Completar\n4. Salir");
-        System.out.print("Opcion: ");
-        opcion = Integer.parseInt(br.readLine());
+        do {
+            System.out.println("APP: TRABAJAVAGO");
+            System.out.println("1. Agregar\n2. Ver\n3. Completar\n4. Salir");
+            System.out.print("Opcion: ");
+            try {
+                opcion = Byte.parseByte(br.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Dato invalido.");
+            } catch (Exception e) {
+                System.out.println("Error desconocido.");
+            }
+
+            switch (opcion) {
+                case 1:
+                    //AQUI
+                    break;
+                case 2:
+                    //ACA
+                    break;
+                case 3:
+                    //ACULLA
+                    break;
+                case 4:
+                    //AHI
+                    break;
+                default:
+                    System.out.println("Opcion fuera de rango");
+                    break;
+            }
+        } while(opcion != 4);
     }
 }
