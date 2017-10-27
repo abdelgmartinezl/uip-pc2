a8484d3fcd24f0164b31f75c2b60e229de07dd19
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-26 19:38:23 -0500

    Clase Artículo final

--- Ejemplos/OOP/src/no/tengo/naa/Articulo.java
+++ Ejemplos/OOP/src/no/tengo/naa/Articulo.java
@@ -15,52 +15,24 @@ public class Articulo {
         this.cantidad = cantidad;
     }
 
-    public void consultar() {
-    }
-
-    public void anadir() {
-    }
-
-    public void borrar() {
-    }
-
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public double getPeso() {
         return peso;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
     }
 
     public double getPrecioUnitario() {
         return precioUnitario;
     }
 
-    public void setPrecioUnitario(double precioUnitario) {
-        this.precioUnitario = precioUnitario;
-    }
-
     public int getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
 }
