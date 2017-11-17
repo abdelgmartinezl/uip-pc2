f736561c81cf11451bc656312f5619a747877d66
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-11-16 20:34:13 -0500

    JavaFX

--- Ejemplos/PrimerWinApp/src/sample/Controller.java
+++ Ejemplos/PrimerWinApp/src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    TextField nombre;
+
+    public void clicar(ActionEvent actionEvent) {
+    }
+
+    public void saludar(ActionEvent actionEvent) {
+        String n = nombre.getText();
+        if (n.length() == 0) {
+            System.out.println("No hay naa!");
+        } else {
+            System.out.println("Bomba y plena, " + n);
+        }
+    }
+
+    public void salir(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
--- Ejemplos/PrimerWinApp/src/sample/Main.java
+++ Ejemplos/PrimerWinApp/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Mi Primer JavaFX App");
+        primaryStage.setTitle("Mi Primer JavaFX App ");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
