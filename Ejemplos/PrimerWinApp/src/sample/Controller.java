701b236dd59b8db4abf9a6a4774676ad2cc95ed5
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-11-16 20:54:11 -0500

    JavaFX

--- Ejemplos/PrimerWinApp/src/sample/Controller.java
+++ Ejemplos/PrimerWinApp/src/sample/Controller.java
@@ -3,14 +3,33 @@ package sample;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
     TextField nombre;
+    @FXML
+    Label cantidad;
 
     public void clicar(ActionEvent actionEvent) {
+        int c = Integer.parseInt(cantidad.getText());
+        c += 1;
+        switch (c) {
+            case 25:
+                Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+                alerta.setTitle("Confirmación de Salida");
+                break;
+            case 50:
+                System.out.println("Ocioso(a)");
+                break;
+            case 100:
+                Platform.exit();
+                break;
+        }
+        cantidad.setText(String.valueOf(c));
     }
 
     public void saludar(ActionEvent actionEvent) {
