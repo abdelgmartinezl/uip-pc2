1f465dcb2eef6868d3499e17692e22162ee777d6
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-02-23 20:56:58 -0500

    Profe y las clases donde van

--- Ejemplos/ejemplo53/Carro.java
+++ Ejemplos/ejemplo53/Carro.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Carro {
     private String marca;
     private String modelo;
@@ -10,14 +7,17 @@ class Carro {
         System.out.println("Arranco el carro");
     }
 
-    public double avanzar(double t, double v) {
+    public double mover(double t, double v) {
         if ( this.tanque_gasolina > 0 ) {
             this.tanque_gasolina -= t*v/10;
             return t*v;
         } else {
-            // Se detiene y se apago
+            apagar();
             return 0.0;
         }
     }
 
+    public void apagar() {
+        System.out.println("Apago el carro");
+    }
 }
