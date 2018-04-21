f39158837370400aad953ef28598ebd4934a06d8
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-04-20 20:53:44 -0500

    Pa lante

--- Ejemplos/ColorCambiante/src/sample/Controller.java
+++ Ejemplos/ColorCambiante/src/sample/Controller.java
@@ -4,19 +4,30 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class Controller {
     @FXML
     ImageView humano;
+    @FXML
+    Circle status;
+    @FXML
+    ImageView cheinchein;
 
     boolean hacker = false;
 
     public void cambiar(MouseEvent mouseEvent) {
         if (!hacker) {
             humano.setImage(new Image("sample/2.png"));
+            status.setFill(Color.GREEN);
+            cheinchein.setVisible(true);
             hacker = true;
         } else {
             humano.setImage(new Image("sample/1.png"));
+            status.setFill(Color.RED);
+            cheinchein.setVisible(false);
             hacker = false;
         }
     }
